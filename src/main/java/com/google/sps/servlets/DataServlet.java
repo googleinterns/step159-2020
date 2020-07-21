dd1d637ebd8a48fdaba6b84ac7dbd205bd4a54c6
Laura <lqueipom@google.com>
2020-07-21 17:36:09 +0000

    adding tentative form

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -1,1 +1,101 @@
+package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** An item on a todo list. */
+
+/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+@WebServlet("/data")
+public class DataServlet extends HttpServlet {
+
+  private final List<Object> json = new ArrayList<>();
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Comments").addSort("score-class", SortDirection.ASCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    List<Entity> results =
+        datastore.prepare(query).asList(FetchOptions.Builder.withLimit(quantity));
+
+    for (Entity entity : results) {
+      String comment = (String) entity.getProperty("comments-class");
+      json.add(comment);
+    }
+
+    // Send JSON string.
+    String jsonVersion = new Gson().toJson(json);
+    response.setContentType("application/json;");
+    response.getWriter().println(jsonVersion);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get written feedback.
+    String classFeedback = request.getParameter("class-input");
+    String professorFeedback = request.getParameter("prof-input");
+    // Get class/professor ratings.
+    String ratingClass = request.getParameter("rating-class");
+    String ratingProfessor = request.getParameter("rating-professor");
+    String language = request.getParameter("languages");
+
+    Document doc =
+        Document.newBuilder().setContent(classFeedback).setType(Document.Type.PLAIN_TEXT).build();
+    LanguageServiceClient languageService = LanguageServiceClient.create();
+    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+    float scoreClass = sentiment.getScore();
+    languageService.close();
+
+    Document doc =
+        Document.newBuilder()
+            .setContent(professorFeedback)
+            .setType(Document.Type.PLAIN_TEXT)
+            .build();
+    LanguageServiceClient languageService = LanguageServiceClient.create();
+    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+    float scoreProfessor = sentiment.getScore();
+    languageService.close();
+
+    Entity professorEntity = new Entity("Professor");
+    taskEntity.setProperty("comments-professor", professorFeedback);
+    taskEntity.setProperty("score-professor", scoreProfessor);
+    taskEntity.setProperty("perception", ratingProfessor);
+
+    Entity commentsEntity = new Entity("Comments");
+    taskEntity.setProperty("perception", ratingClass);
+    taskEntity.setProperty("comments-class", classFeedback);
+    taskEntity.setProperty("score-class", scoreClass);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentsEntity);
+    datastore.put(professorEntity);
+
+    response.setContentType("text/html; charset=UTF-8");
+    response.setCharacterEncoding("UTF-8");
+    response.sendRedirect("/index.html");
+  }
+
+  /**
+   * @return the request parameter, or the default value if the parameter was not specified by the
+   *     client
+   */
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    return value != null ? value : defaultValue;
+  }
+}
