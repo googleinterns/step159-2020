4f0c82da2916af6c16b09b3f5e822694592ad056
Nina Prabhu <nsp@stanford.edu>
2020-07-24 21:48:50 +0000

    Add more search filtering

--- src/main/java/com/google/sps/servlets/SearchServlet.java
+++ src/main/java/com/google/sps/servlets/SearchServlet.java
@@ -19,12 +19,14 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -41,10 +43,42 @@ public class SearchServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     List<String> classNames = new ArrayList<>();
     List<String> professorNames = new ArrayList<>();
-    String profName = request.getParameter("profName");
+    List<Filter> filters = new ArrayList<>();
+    if (!request.getParameter("className").isEmpty()) {
+      String className = request.getParameter("className");
+      Filter classFilter = new FilterPredicate("Name", FilterOperator.EQUAL, className);
+      filters.add(classFilter);
+    }
+    if (!request.getParameter("profName").isEmpty()) {
+      String profName = request.getParameter("profName");
+      Filter profFilter = new FilterPredicate("Professor", FilterOperator.EQUAL, profName);
+      filters.add(profFilter);
+    }
+    List<Integer> units = new ArrayList<>();
+    if (!request.getParameter("units").isEmpty()) {
+      List<String> strUnits = Arrays.asList(request.getParameter("units").split(","));
+      for (String number : strUnits) {
+        units.add(Integer.valueOf(number));
+      }
+      if (units.size() != 0) {
+        Filter unitsFilter = new FilterPredicate("Units", FilterOperator.IN, units);
+        filters.add(unitsFilter);
+      }
+    }
+
+    Query q = new Query("Class");
+    // Add filters depending on number of fields specified
+    if (filters.size() == 1) {
+      q.setFilter(filters.get(0));
+    } else if (filters.size() == 2) {
+      q.setFilter(CompositeFilterOperator.and(filters.get(0), filters.get(1)));
+    } else if (filters.size() == 3) {
+      q.setFilter(
+          CompositeFilterOperator.and(
+              CompositeFilterOperator.and(filters.get(0), filters.get(1), filters.get(2))));
+    }
+
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Filter propertyFilter = new FilterPredicate("Professor", FilterOperator.EQUAL, profName);
-    Query q = new Query("Class").setFilter(propertyFilter);
     List<Entity> classes = datastore.prepare(q).asList(FetchOptions.Builder.withDefaults());
     for (Entity entity : classes) {
       String name = (String) entity.getProperty("Name");
@@ -52,6 +86,8 @@ public class SearchServlet extends HttpServlet {
       String professor = (String) entity.getProperty("Professor");
       professorNames.add(professor);
     }
+
+    // Generate links for class pages and send them back - set variables in URL
     String classNameJson = new Gson().toJson(classNames);
     String profNameJson = new Gson().toJson(professorNames);
     JSONObject obj = new JSONObject();
@@ -66,10 +102,13 @@ public class SearchServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String name = request.getParameter("class-name");
     String prof = request.getParameter("prof-name");
+    Integer units = Integer.valueOf(request.getParameter("num-units"));
+
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Entity newClass = new Entity("Class");
     newClass.setProperty("Name", name);
     newClass.setProperty("Professor", prof);
+    newClass.setProperty("Units", units);
     datastore.put(newClass);
     response.sendRedirect("/search.html");
   }
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -18,12 +18,27 @@ function verifyLogin() {
   }));
 }
 
+function countUnits(){
+  const inputElems = document.getElementsByName("search-units");
+  const arr = []
+  for (let i=0; i<inputElems.length; i++) {       
+    if (inputElems[i].checked){
+      arr.push(inputElems[i].value);
+    }
+  }
+  return arr;
+}
+
 function showClasses() {
   const classes = document.getElementById("search-results");
+  const className = document.getElementById("search-class").value;
   const profName = document.getElementById("search-prof").value;
+  const units = countUnits();
   classes.innerHTML = "";
   const url = new URL("/search", window.location.origin);
+  url.searchParams.set("className", className);
   url.searchParams.set("profName", profName);
+  url.searchParams.set("units", units);
   fetch(url)
   .then(response => response.json())
   .then((response) => {
