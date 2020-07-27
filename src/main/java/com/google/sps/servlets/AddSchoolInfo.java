7e0038a275a01af40d42bac442bfb4425415ab24
Dagmawi <dagmawi.s.haile@gmail.com>
2020-07-27 22:30:02 +0000

    simplifies code, unifies style

--- src/main/java/com/google/sps/servlets/AddSchoolInfo.java
+++ src/main/java/com/google/sps/servlets/AddSchoolInfo.java
@@ -9,7 +9,6 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.sps.data.PostRequestObject;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
@@ -18,7 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/add-info")
-public class addSchoolInfo extends HttpServlet {
+public class AddSchoolInfo extends HttpServlet {
 
   private Key existingSchoolKey;
   private Key existingCourseKey;
@@ -32,22 +31,21 @@ public class addSchoolInfo extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    PostRequestObject postRequestData = processPostRequest(request);
-    String schoolName = postRequestData.getSchool();
-    String courseName = postRequestData.getCourse();
-    String term = postRequestData.getTerm();
-    String units = postRequestData.getUnits();
-    String profName = postRequestData.getProf();
+    String schoolName = request.getParameter("school-name");
+    String courseName = request.getParameter("course-name");
+    String term = request.getParameter("term");
+    Integer units = Integer.parseInt(request.getParameter("units"));
+    String profName = request.getParameter("professor-name");
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
     Filter schoolFilter = new FilterPredicate("school-name", FilterOperator.EQUAL, schoolName);
-    Query schoolQ = new Query("School").setFilter(schoolFilter);
+    Query schoolQ = new Query("school").setFilter(schoolFilter);
     List<Entity> schoolQuery =
         datastore.prepare(schoolQ).asList(FetchOptions.Builder.withDefaults());
 
-    if (schoolQuery.size() == 0) {
-      Entity newSchool = new Entity("School");
+    if (schoolQuery.isEmpty()) {
+      Entity newSchool = new Entity("school");
       newSchool.setProperty("school-name", schoolName);
       newSchoolKey = newSchool.getKey();
       datastore.put(newSchool);
@@ -58,62 +56,59 @@ public class addSchoolInfo extends HttpServlet {
     }
 
     if (newSchoolKey != null) {
-      Entity newCourse = new Entity("Course", newSchoolKey);
+      Entity newCourse = new Entity("course", newSchoolKey);
       newCourse.setProperty("course-name", courseName);
-      newCourse.setProperty("Units", units);
+      newCourse.setProperty("units", units);
       newCourseKey = newCourse.getKey();
       datastore.put(newCourse);
 
-      Entity newProfessor;
-      newProfessor = new Entity("Professor", newSchoolKey);
+      Entity newProfessor = new Entity("professor", newSchoolKey);
       newProfessor.setProperty("professor-name", profName);
       newProfessorKey = newProfessor.getKey();
       datastore.put(newProfessor);
 
-      Entity newTerm = new Entity("Term", newCourseKey);
+      Entity newTerm = new Entity("term", newCourseKey);
       newTerm.setProperty("term", term);
-      newTerm.setProperty("Professor", newProfessorKey);
+      newTerm.setProperty("professor", newProfessorKey);
       datastore.put(newTerm);
 
     } else {
       Filter courseFilter = new FilterPredicate("course-name", FilterOperator.EQUAL, courseName);
-      Query courseQ = new Query("Course").setAncestor(existingSchoolKey).setFilter(courseFilter);
+      Query courseQ = new Query("course").setAncestor(existingSchoolKey).setFilter(courseFilter);
       List<Entity> courseQuery =
           datastore.prepare(courseQ).asList(FetchOptions.Builder.withDefaults());
 
       // existing school --> new course
-      if (courseQuery.size() == 0) {
+      if (courseQuery.isEmpty()) {
         Entity newCourse;
         if (existingSchoolKey != null) {
-          newCourse = new Entity("Course", existingSchoolKey);
+          newCourse = new Entity("course", existingSchoolKey);
         } else {
-          newCourse = new Entity("Course", newSchoolKey);
+          newCourse = new Entity("course", newSchoolKey);
         }
 
         newCourse.setProperty("course-name", courseName);
-        newCourse.setProperty("Units", units);
+        newCourse.setProperty("units", units);
         datastore.put(newCourse);
 
-        Entity newTerm = new Entity("Term", newCourseKey);
+        Entity newTerm = new Entity("term", newCourseKey);
         newTerm.setProperty("term", term);
 
         Filter professorFilter =
             new FilterPredicate("professor-name", FilterOperator.EQUAL, profName);
         Query professorQ =
-            new Query("Professor").setAncestor(existingSchoolKey).setFilter(professorFilter);
+            new Query("professor").setAncestor(existingSchoolKey).setFilter(professorFilter);
         List<Entity> professorQuery =
             datastore.prepare(professorQ).asList(FetchOptions.Builder.withDefaults());
 
         // existing school --> new course --> new professor
-        if (professorQuery.size() == 0) {
-          System.out.println("OLD SCHOOL NEW COURSE NEW PROFESSOR");
-          Entity newProfessor;
-          newProfessor = new Entity("Professor", existingSchoolKey);
+        if (professorQuery.isEmpty()) {
+          Entity newProfessor = new Entity("professor", existingSchoolKey);
           newProfessor.setProperty("professor-name", profName);
           newProfessorKey = newProfessor.getKey();
           datastore.put(newProfessor);
 
-          newTerm.setProperty("Professor", newProfessorKey);
+          newTerm.setProperty("professor", newProfessorKey);
           datastore.put(newTerm);
 
         } else {
@@ -121,7 +116,7 @@ public class addSchoolInfo extends HttpServlet {
           Entity existingProfessor = professorQuery.get(0);
           existingProfessorKey = existingProfessor.getKey();
 
-          newTerm.setProperty("Professor", newProfessorKey);
+          newTerm.setProperty("professor", newProfessorKey);
           datastore.put(newTerm);
         }
       } else {
@@ -129,51 +124,36 @@ public class addSchoolInfo extends HttpServlet {
         Entity existingCourse = courseQuery.get(0);
         existingCourseKey = existingCourse.getKey();
 
-        Entity newTerm = new Entity("Term", existingCourseKey);
+        Entity newTerm = new Entity("term", existingCourseKey);
         newTerm.setProperty("term", term);
 
         Filter professorFilter =
             new FilterPredicate("professor-name", FilterOperator.EQUAL, profName);
         Query professorQ =
-            new Query("Professor").setAncestor(existingSchoolKey).setFilter(professorFilter);
+            new Query("professor").setAncestor(existingSchoolKey).setFilter(professorFilter);
         List<Entity> professorQuery =
             datastore.prepare(professorQ).asList(FetchOptions.Builder.withDefaults());
 
         // existing school --> existing course --> new professor
         if (professorQuery.size() == 0) {
-          Entity newProfessor;
-          newProfessor = new Entity("Professor", existingSchoolKey);
+          Entity newProfessor = new Entity("professor", existingSchoolKey);
           newProfessor.setProperty("professor-name", profName);
           newProfessorKey = newProfessor.getKey();
           datastore.put(newProfessor);
-
-          newTerm.setProperty("Professor", newProfessorKey);
+          newTerm.setProperty("professor", newProfessorKey);
           datastore.put(newTerm);
 
         } else {
           // existing school --> existing course --> existing professor
           Entity existingProfessor = professorQuery.get(0);
           existingProfessorKey = existingProfessor.getKey();
-          newTerm.setProperty("Professor", existingProfessorKey);
+          newTerm.setProperty("professor", existingProfessorKey);
           datastore.put(newTerm);
         }
       }
     }
-
     response.setContentType("text/html; charset=UTF-8");
     response.setCharacterEncoding("UTF-8");
     response.sendRedirect("/addSchoolInfo.html");
-  }
-
-  public PostRequestObject processPostRequest(HttpServletRequest request) {
-    String schoolName = request.getParameter("school-name");
-    String courseName = request.getParameter("course-name");
-    String term = request.getParameter("term");
-    String units = request.getParameter("units");
-    String profName = request.getParameter("professor-name");
-
-    PostRequestObject newPost =
-        new PostRequestObject(schoolName, courseName, term, units, profName);
-    return newPost;
   }
 }
