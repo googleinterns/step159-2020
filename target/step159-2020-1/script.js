3a29821502e6b6223e0afda79eec569f1afd1c84
Dagmawi <dagmawi.s.haile@gmail.com>
2020-07-27 23:50:37 +0000

    deletes untracked files

--- target/step159-2020-1/script.js
+++
@@ -1,8 +0,0 @@
-function makeGraph() {
-  fetch('/graph')
-    .then((response) => response.json())
-    .then((data) => {
-      dataObject = JSON.parse(data);
-      console.log(dataObject);
-    });
-}
