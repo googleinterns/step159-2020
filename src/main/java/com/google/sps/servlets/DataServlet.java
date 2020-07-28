47c056b5a2b86fbba6e5f724104b05158d83eefd
Laura <lqueipom@gmail.com>
2020-07-28 15:58:40 +0000

    Adding functions that store parameters in URL

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -1,182 +1,188 @@
-package com.google.sps.servlets;
+// package com.google.sps.servlets;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.cloud.language.v1.Document;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.Sentiment;
-import com.google.cloud.translate.Translate;
-import com.google.cloud.translate.TranslateOptions;
-import com.google.cloud.translate.Translation;
-import com.google.gson.Gson;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+// import com.google.appengine.api.datastore.DatastoreService;
+// import com.google.appengine.api.datastore.DatastoreServiceFactory;
+// import com.google.appengine.api.datastore.Entity;
+// import com.google.appengine.api.datastore.FetchOptions;
+// import com.google.appengine.api.datastore.Key;
+// import com.google.appengine.api.datastore.PreparedQuery;
+// import com.google.appengine.api.datastore.Query;
+// import com.google.appengine.api.datastore.Query.Filter;
+// import com.google.appengine.api.datastore.Query.FilterOperator;
+// import com.google.appengine.api.datastore.Query.FilterPredicate;
+// import com.google.appengine.api.datastore.Query.SortDirection;
+// import com.google.appengine.api.users.UserService;
+// import com.google.appengine.api.users.UserServiceFactory;
+// import com.google.cloud.language.v1.Document;
+// import com.google.cloud.language.v1.LanguageServiceClient;
+// import com.google.cloud.language.v1.Sentiment;
+// import com.google.cloud.translate.Translate;
+// import com.google.cloud.translate.TranslateOptions;
+// import com.google.cloud.translate.Translation;
+// import com.google.gson.Gson;
+// import java.io.IOException;
+// import java.util.ArrayList;
+// import java.util.List;
+// import javax.servlet.annotation.WebServlet;
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
 
-/** An item on a todo list. */
+// /** An item on a todo list. */
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
-public class DataServlet extends HttpServlet {
-  private final List<Object> commentsList = new ArrayList<>();
-  private Key existingTermRatingKey;
-  private Key currentProfKey;
-  private Key currentTermKey;
+// /** Servlet that returns some example content. TODO: modify this file to handle comments data */
+// @WebServlet("/data")
+// public class DataServlet extends HttpServlet {
+//   private final List<Object> commentsList = new ArrayList<>();
+//   private Key existingTermRatingKey;
+//   private Key currentProfKey;
+//   private Key currentTermKey;
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Query query = new Query("Comments").addSort("score-class", SortDirection.ASCENDING);
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery results = datastore.prepare(query);
+//   @Override
+//   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+// {
+//     Query query = new Query("Comments").addSort("score-class", SortDirection.ASCENDING);
+//     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+//     PreparedQuery results = datastore.prepare(query);
 
-    for (Entity entity : results.asIterable()) {
-      String comment = (String) entity.getProperty("comments-class");
-      commentsList.add(comment);
-    }
+//     for (Entity entity : results.asIterable()) {
+//       String comment = (String) entity.getProperty("comments-class");
+//       commentsList.add(comment);
+//     }
 
-    // Send JSON string.
-    String jsonVersionCommentsList = new Gson().toJson(commentsList);
-    commentsList.clear();
-    response.setContentType("application/json;");
-    response.getWriter().println(jsonVersionCommentsList);
-  }
+//     // Send JSON string.
+//     String jsonVersionCommentsList = new Gson().toJson(commentsList);
+//     commentsList.clear();
+//     response.setContentType("application/json;");
+//     response.getWriter().println(jsonVersionCommentsList);
+//   }
 
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Get written feedback.
-    String classFeedback = request.getParameter("class-input");
-    int classRating = Integer.parseInt(request.getParameter("rating-class"));
-    int workHours = Integer.parseInt(request.getParameter("hoursOfWork"));
-    int difficulty = Integer.parseInt(request.getParameter("difficulty"));
-    String professorFeedback = request.getParameter("prof-input");
-    int professorRating = Integer.parseInt(request.getParameter("rating-professor"));
-    boolean translateToEnglish = Boolean.parseBoolean(request.getParameter("languages"));
+//   @Override
+//   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+// {
+//     // Get written feedback.
+//     String classFeedback = request.getParameter("class-input");
+//     int classRating = Integer.parseInt(request.getParameter("rating-class"));
+//     int workHours = Integer.parseInt(request.getParameter("hoursOfWork"));
+//     int difficulty = Integer.parseInt(request.getParameter("difficulty"));
+//     String professorFeedback = request.getParameter("prof-input");
+//     int professorRating = Integer.parseInt(request.getParameter("rating-professor"));
+//     boolean translateToEnglish = Boolean.parseBoolean(request.getParameter("languages"));
 
-    if (translateToEnglish) {
-      Translate translateService = TranslateOptions.getDefaultInstance().getService();
-      Translation translationClassFeedback =
-          translateService.translate(classFeedback, Translate.TranslateOption.targetLanguage("en"));
-      Translation translationProfessorFeedback =
-          translateService.translate(
-              professorFeedback, Translate.TranslateOption.targetLanguage("en"));
-      String translatedClassFeedback = translationClassFeedback.getTranslatedText();
-      String translatedProfessorFeedback = translationProfessorFeedback.getTranslatedText();
-      classFeedback = translatedClassFeedback;
-      professorFeedback = translatedProfessorFeedback;
-    }
+//     if (translateToEnglish) {
+//       Translate translateService = TranslateOptions.getDefaultInstance().getService();
+//       Translation translationClassFeedback =
+//           translateService.translate(classFeedback,
+// Translate.TranslateOption.targetLanguage("en"));
+//       Translation translationProfessorFeedback =
+//           translateService.translate(
+//               professorFeedback, Translate.TranslateOption.targetLanguage("en"));
+//       String translatedClassFeedback = translationClassFeedback.getTranslatedText();
+//       String translatedProfessorFeedback = translationProfessorFeedback.getTranslatedText();
+//       classFeedback = translatedClassFeedback;
+//       professorFeedback = translatedProfessorFeedback;
+//     }
 
-    Document classFeedbackDoc =
-        Document.newBuilder().setContent(classFeedback).setType(Document.Type.PLAIN_TEXT).build();
-    Document professorFeedbackDoc =
-        Document.newBuilder()
-            .setContent(professorFeedback)
-            .setType(Document.Type.PLAIN_TEXT)
-            .build();
+//     Document classFeedbackDoc =
+//
+// Document.newBuilder().setContent(classFeedback).setType(Document.Type.PLAIN_TEXT).build();
+//     Document professorFeedbackDoc =
+//         Document.newBuilder()
+//             .setContent(professorFeedback)
+//             .setType(Document.Type.PLAIN_TEXT)
+//             .build();
 
-    LanguageServiceClient classLanguageService = LanguageServiceClient.create();
-    Sentiment classSentiment =
-        classLanguageService.analyzeSentiment(classFeedbackDoc).getDocumentSentiment();
-    float classScore = classSentiment.getScore();
-    classLanguageService.close();
+//     LanguageServiceClient classLanguageService = LanguageServiceClient.create();
+//     Sentiment classSentiment =
+//         classLanguageService.analyzeSentiment(classFeedbackDoc).getDocumentSentiment();
+//     float classScore = classSentiment.getScore();
+//     classLanguageService.close();
 
-    LanguageServiceClient profLanguageService = LanguageServiceClient.create();
-    Sentiment professorSentiment =
-        profLanguageService.analyzeSentiment(professorFeedbackDoc).getDocumentSentiment();
-    float professorScore = professorSentiment.getScore();
-    profLanguageService.close();
+//     LanguageServiceClient profLanguageService = LanguageServiceClient.create();
+//     Sentiment professorSentiment =
+//         profLanguageService.analyzeSentiment(professorFeedbackDoc).getDocumentSentiment();
+//     float professorScore = professorSentiment.getScore();
+//     profLanguageService.close();
 
-    // Gets user email.
-    UserService userService = UserServiceFactory.getUserService();
-    String userEmail = userService.getCurrentUser().getEmail();
+//     // Gets user email.
+//     UserService userService = UserServiceFactory.getUserService();
+//     String userEmail = userService.getCurrentUser().getEmail();
 
-    // Check whether user has reviewer ID in system.
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Filter userFilter = new FilterPredicate("user-email", FilterOperator.EQUAL, userEmail);
-    Query userQuery = new Query("User").setFilter(userFilter);
-    List<Entity> userQueryList =
-        datastore.prepare(userQuery).asList(FetchOptions.Builder.withDefaults());
-    Entity userID = new Entity("User");
+//     // Check whether user has reviewer ID in system.
+//     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+//     Filter userFilter = new FilterPredicate("user-email", FilterOperator.EQUAL, userEmail);
+//     Query userQuery = new Query("User").setFilter(userFilter);
+//     List<Entity> userQueryList =
+//         datastore.prepare(userQuery).asList(FetchOptions.Builder.withDefaults());
+//     Entity userID = new Entity("User");
 
-    // User has not reviewed any rating.
-    if (userQueryList.size() == 0) {
-      Entity newReviewer = new Entity("User");
-      newReviewer.setProperty("user-email", userEmail);
-      userID = newReviewer;
-    } else {
-      userID = userQueryList.get(0);
-    }
+//     // User has not reviewed any rating.
+//     if (userQueryList.size() == 0) {
+//       Entity newReviewer = new Entity("User");
+//       newReviewer.setProperty("user-email", userEmail);
+//       userID = newReviewer;
+//     } else {
+//       userID = userQueryList.get(0);
+//     }
 
-    currentTermKey = request.getParameter("term");
-    Entity currentTerm = datastore.get(currentTermKey);
-    currentProfKey = currentTerm.getProperty("Professor");
+//     currentTermKey = request.getParameter("term");
+//     Entity currentTerm = datastore.get(currentTermKey);
+//     currentProfKey = currentTerm.getProperty("Professor");
 
-    // Checks for term rating, only need to query one as prof and term get reviewed simultaneously.
-    Filter reviewerFilter =
-        new FilterPredicate("reviewer-id", FilterOperator.EQUAL, userID.getKey());
-    Query ratingQuery = new Query("Rating").setAncestor(currentTermKey).setFilter(reviewerFilter);
-    List<Entity> ratingQueryList =
-        datastore.prepare(ratingQuery).asList(FetchOptions.Builder.withDefaults());
+//     // Checks for term rating, only need to query one as prof and term get reviewed
+// simultaneously.
+//     Filter reviewerFilter =
+//         new FilterPredicate("reviewer-id", FilterOperator.EQUAL, userID.getKey());
+//     Query ratingQuery = new
+// Query("Rating").setAncestor(currentTermKey).setFilter(reviewerFilter);
+//     List<Entity> ratingQueryList =
+//         datastore.prepare(ratingQuery).asList(FetchOptions.Builder.withDefaults());
 
-    if (ratingQueryList.size() != 0) {
-      // A rating from this user for this term and professor has been found.
-      Entity existingTermRatingEntity = ratingQueryList.get(0);
-      existingTermRatingKey = existingTermRatingEntity.getKey();
+//     if (ratingQueryList.size() != 0) {
+//       // A rating from this user for this term and professor has been found.
+//       Entity existingTermRatingEntity = ratingQueryList.get(0);
+//       existingTermRatingKey = existingTermRatingEntity.getKey();
 
-      Entity updatedProfRatingEntity = datastore.get(currentProfKey);
-      updatedProfRatingEntity.setProperty("comments-professor", professorFeedback);
-      updatedProfRatingEntity.setProperty("score-professor", professorScore);
-      updatedProfRatingEntity.setProperty("perception", professorRating);
-      updatedProfRatingEntity.setProperty("difficulty", difficulty);
-      updatedProfRatingEntity.setProperty("hours", workHours);
+//       Entity updatedProfRatingEntity = datastore.get(currentProfKey);
+//       updatedProfRatingEntity.setProperty("comments-professor", professorFeedback);
+//       updatedProfRatingEntity.setProperty("score-professor", professorScore);
+//       updatedProfRatingEntity.setProperty("perception", professorRating);
+//       updatedProfRatingEntity.setProperty("difficulty", difficulty);
+//       updatedProfRatingEntity.setProperty("hours", workHours);
 
-      Entity updatedTermRatingEntity = datastore.get(existingTermRatingKey);
-      updatedTermRatingEntity.setProperty("comments-class", classFeedback);
-      updatedTermRatingEntity.setProperty("score-class", classScore);
-      updatedTermRatingEntity.setProperty("perception", classRating);
-      updatedTermRatingEntity.setProperty("difficulty", difficulty);
-      updatedTermRatingEntity.setProperty("hours", workHours);
+//       Entity updatedTermRatingEntity = datastore.get(existingTermRatingKey);
+//       updatedTermRatingEntity.setProperty("comments-class", classFeedback);
+//       updatedTermRatingEntity.setProperty("score-class", classScore);
+//       updatedTermRatingEntity.setProperty("perception", classRating);
+//       updatedTermRatingEntity.setProperty("difficulty", difficulty);
+//       updatedTermRatingEntity.setProperty("hours", workHours);
 
-      datastore.put(updatedTermRatingEntity);
-      datastore.put(updatedProfRatingEntity);
-    } else {
-      // A rating from this user has not been found, will be created.
-      Entity professorRatingEntity = new Entity("Rating", currentProfKey);
-      professorRatingEntity.setProperty("comments-professor", professorFeedback);
-      professorRatingEntity.setProperty("reviewer-id", userID.getKey());
-      professorRatingEntity.setProperty("score-professor", professorScore);
-      professorRatingEntity.setProperty("perception", professorRating);
-      professorRatingEntity.setProperty("hours", workHours);
-      professorRatingEntity.setProperty("difficulty", difficulty);
+//       datastore.put(updatedTermRatingEntity);
+//       datastore.put(updatedProfRatingEntity);
+//     } else {
+//       // A rating from this user has not been found, will be created.
+//       Entity professorRatingEntity = new Entity("Rating", currentProfKey);
+//       professorRatingEntity.setProperty("comments-professor", professorFeedback);
+//       professorRatingEntity.setProperty("reviewer-id", userID.getKey());
+//       professorRatingEntity.setProperty("score-professor", professorScore);
+//       professorRatingEntity.setProperty("perception", professorRating);
+//       professorRatingEntity.setProperty("hours", workHours);
+//       professorRatingEntity.setProperty("difficulty", difficulty);
 
-      Entity classRatingEntity = new Entity("Rating", currentTermKey);
-      classRatingEntity.setProperty("comments-class", classFeedback);
-      classRatingEntity.setProperty("reviewer-id", userID.getKey());
-      classRatingEntity.setProperty("score-class", classScore);
-      classRatingEntity.setProperty("perception", classRating);
-      classRatingEntity.setProperty("hours", workHours);
-      classRatingEntity.setProperty("difficulty", difficulty);
+//       Entity classRatingEntity = new Entity("Rating", currentTermKey);
+//       classRatingEntity.setProperty("comments-class", classFeedback);
+//       classRatingEntity.setProperty("reviewer-id", userID.getKey());
+//       classRatingEntity.setProperty("score-class", classScore);
+//       classRatingEntity.setProperty("perception", classRating);
+//       classRatingEntity.setProperty("hours", workHours);
+//       classRatingEntity.setProperty("difficulty", difficulty);
 
-      datastore.put(classRatingEntity);
-      datastore.put(professorRatingEntity);
-    }
+//       datastore.put(classRatingEntity);
+//       datastore.put(professorRatingEntity);
+//     }
 
-    response.setContentType("text/html; charset=UTF-8");
-    response.setCharacterEncoding("UTF-8");
-    response.sendRedirect("/index.html");
-  }
-}
+//     response.setContentType("text/html; charset=UTF-8");
+//     response.setCharacterEncoding("UTF-8");
+//     response.sendRedirect("/index.html");
+//   }
+// }
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -7,29 +7,35 @@ function makeGraph() {
     .then((data) => {
       const hourData = new google.visualization.arrayToDataTable(data.hours);
       const hourOptions = {
-          title: "Hours Spent per Week",
-          legend: { position: "none" },
-          vAxis: {title: "# Students"},
-          hAxis: {title: "Hours"},
-          histogram: {
-            hideBucketItems: true
-          }
-        };
-        const hourChart = new google.visualization.Histogram(document.getElementById("hours-chart"));
-        hourChart.draw(hourData, hourOptions);
+        title: "Hours Spent per Week",
+        legend: { position: "none" },
+        vAxis: { title: "# Students" },
+        hAxis: { title: "Hours" },
+        histogram: {
+          hideBucketItems: true,
+        },
+      };
+      const hourChart = new google.visualization.Histogram(
+        document.getElementById("hours-chart")
+      );
+      hourChart.draw(hourData, hourOptions);
 
-      const difficultyData = new google.visualization.arrayToDataTable(data.difficulty);
+      const difficultyData = new google.visualization.arrayToDataTable(
+        data.difficulty
+      );
       const diffOptions = {
-          title: "Diffculty of Class",
-          legend: { position: "none" },
-          vAxis: {title: "# Students"},
-          hAxis: {title: "Difficulty"},
-          histogram: {
-            hideBucketItems: true,
-          }
-        };
-        const diffChart = new google.visualization.Histogram(document.getElementById("difficulty-chart"));
-        diffChart.draw(difficultyData, diffOptions);
+        title: "Diffculty of Class",
+        legend: { position: "none" },
+        vAxis: { title: "# Students" },
+        hAxis: { title: "Difficulty" },
+        histogram: {
+          hideBucketItems: true,
+        },
+      };
+      const diffChart = new google.visualization.Histogram(
+        document.getElementById("difficulty-chart")
+      );
+      diffChart.draw(difficultyData, diffOptions);
     });
 }
 
@@ -39,21 +45,23 @@ function loadBody() {
 
 /** Contacts Login servlet to verify if user is logged in. */
 function verifyLogin() {
-  fetch('/login').then(response => response.json()).then((userDetails => {
-    const loginElement = document.getElementById("login-box");
-    const schoolName = document.getElementById("school-name");
-    if (!userDetails.loggedIn) {
-      schoolName.style.display = "none";
-      loginElement.innerHTML += `<p><a href= ${userDetails.loginURL} > Click here to log in! </a></p>`;
-    } else {
-      console.log(userDetails);
-      schoolName.style.display = "block";
-      if (userDetails.hasOwnProperty("schoolName")){
-        schoolName.innerHTML += userDetails.schoolName;
+  fetch("/login")
+    .then((response) => response.json())
+    .then((userDetails) => {
+      const loginElement = document.getElementById("login-box");
+      const schoolName = document.getElementById("school-name");
+      if (!userDetails.loggedIn) {
+        schoolName.style.display = "none";
+        loginElement.innerHTML += `<p><a href= ${userDetails.loginURL} > Click here to log in! </a></p>`;
+      } else {
+        console.log(userDetails);
+        schoolName.style.display = "block";
+        if (userDetails.hasOwnProperty("schoolName")) {
+          schoolName.innerHTML += userDetails.schoolName;
+        }
+        loginElement.innerHTML += `<p><a href= ${userDetails.logoutURL} + > Click here to log out! </a></p>`;
       }
-      loginElement.innerHTML += `<p><a href= ${userDetails.logoutURL} + > Click here to log out! </a></p>`;
-    }
-  }));
+    });
 }
 
 function showClasses() {
@@ -63,18 +71,20 @@ function showClasses() {
   const url = new URL("/search", window.location.origin);
   url.searchParams.set("profName", profName);
   fetch(url)
-  .then(response => response.json())
-  .then((response) => {
-    return JSON.parse(response.classNames);
-  })
-  .then((classNames) => {
-    classNames.forEach(name => classes.appendChild(createListElement(name)));
-  });
+    .then((response) => response.json())
+    .then((response) => {
+      return JSON.parse(response.classNames);
+    })
+    .then((classNames) => {
+      classNames.forEach((name) =>
+        classes.appendChild(createListElement(name))
+      );
+    });
 }
 
 /* Creates an <li> element containing text. */
 function createListElement(text) {
-  const liElement = document.createElement('li');
+  const liElement = document.createElement("li");
   liElement.innerText = text;
   return liElement;
 }
@@ -84,4 +94,43 @@ function createListElement(text) {
 function getClassID() {
   var classId = Math.floor(Math.random() * 101);
   document.getElementById("class-id").innerHTML = classId;
+}
+
+function setParam(name, value) {
+  var currentUrl = window.location;
+  var parameters = {};
+  var symbols = /(?:\??)([^=&?]+)=?([^&?]*)/g;
+  var queryString = currentUrl.search;
+
+  // Goes through all matches between symbols and the queryString.
+  for (var i = symbols.exec(queryString); i; i = symbols.exec(queryString)) {
+    // Decodes URL.
+    i[1] = decodeURIComponent(i[1]);
+    if (!i[2]) {
+      i[2] = "%%";
+    }
+    parameters[i[1]] = i[2];
+  }
+  parameters[name] = encodeURIComponent(value);
+  var search = [];
+  for (var j in parameters) {
+    var encodedElement = encodeURIComponent(j);
+    var symbol = parameters[j];
+    if (symbol != "%%") {
+      encodedElement += "=" + symbol;
+    }
+    search.push(encodedElement);
+  }
+  search = search.join("&");
+  currentUrl.search = search;
+}
+
+function setAllParams() {
+  var schoolName = document.getElementById("school-name").value;
+  var className = document.getElementById("class-name").value;
+  var profName = document.getElementById("prof-name").value;
+
+  setParam("school", schoolName);
+  setParam("class", className);
+  setParam("professor", profName);
 }
