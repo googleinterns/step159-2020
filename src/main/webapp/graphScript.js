50ddfb9c3f21e3618eff2c1f0051e11545acd1d8
Dagmawi <dagmawi.s.haile@gmail.com>
2020-07-30 19:46:51 +0000

    moves files around

---
+++ src/main/webapp/graphScript.js
@@ -0,0 +1,34 @@
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(makeGraph);
+
+function makeGraph() {
+  fetch("/graph")
+    .then((response) => response.json())
+    .then((data) => {
+      const hourData = new google.visualization.arrayToDataTable(data.hours);
+      const hourOptions = {
+          title: "Hours Spent per Week",
+          legend: { position: "none" },
+          vAxis: {title: "# Students"},
+          hAxis: {title: "Hours"},
+          histogram: {
+            hideBucketItems: true
+          }
+        };
+        const hourChart = new google.visualization.Histogram(document.getElementById("hours-chart"));
+        hourChart.draw(hourData, hourOptions);
+
+      const difficultyData = new google.visualization.arrayToDataTable(data.difficulty);
+      const diffOptions = {
+          title: "Diffculty of Class",
+          legend: { position: "none" },
+          vAxis: {title: "# Students"},
+          hAxis: {title: "Difficulty"},
+          histogram: {
+            hideBucketItems: true,
+          }
+        };
+        const diffChart = new google.visualization.Histogram(document.getElementById("difficulty-chart"));
+        diffChart.draw(difficultyData, diffOptions);
+    });
+}
