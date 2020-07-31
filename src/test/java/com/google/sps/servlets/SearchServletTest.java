275a9c2f4865dfbcdde560d5feb77c701c7c0bde
Nina Prabhu <nsp@stanford.edu>
2020-07-31 21:28:42 +0000

    Fix test style

--- src/test/java/com/google/sps/servlets/SearchServletTest.java
+++ src/test/java/com/google/sps/servlets/SearchServletTest.java
@@ -26,6 +26,7 @@ import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.AfterEach;
@@ -59,77 +60,46 @@ public final class SearchServletTest {
 
   @Test
   /* Ensure the correct filters are set when all search parameters are specified. */
-  public void makeFiltersAllParams() {
-
-    request = Mockito.mock(HttpServletRequest.class);
-
-    when(request.getParameter("courseName")).thenReturn("CS 105");
-    when(request.getParameter("profName")).thenReturn("Smith");
-    when(request.getParameter("term")).thenReturn("Spring 2020");
-    when(request.getParameter("units")).thenReturn("1,2,3");
-
-    List<Filter> filters = searchObject.getFilters(request);
+  public void MakeFilters_AllParamsSet() {
+    request = createRequest(request, "CS 105", "Smith", "Spring 2020", "1,2,3");
 
     List<Filter> expectedFilters = new ArrayList<>();
-    List<Integer> units = new ArrayList<>();
-    units.add(1);
-    units.add(2);
-    units.add(3);
+    List<Integer> units = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
+
     expectedFilters.add(new FilterPredicate("name", FilterOperator.EQUAL, "CS 105"));
     expectedFilters.add(new FilterPredicate("professor", FilterOperator.EQUAL, "Smith"));
     expectedFilters.add(new FilterPredicate("term", FilterOperator.EQUAL, "Spring 2020"));
     expectedFilters.add(new FilterPredicate("units", FilterOperator.IN, units));
+
+    List<Filter> filters = searchObject.getFilters(request);
 
     assertEquals(filters, expectedFilters);
   }
 
   @Test
   /* Ensure no filters are made given no search parameters are specified. */
-  public void makeFiltersNoParams() {
-
-    request = Mockito.mock(HttpServletRequest.class);
+  public void MakeFilters_NoParamsSet() {
 
     // TODO: Find a better way to replicate null parameters -
     // getting NullPointerException when trying other methods.
-    when(request.getParameter("courseName")).thenReturn("");
-    when(request.getParameter("profName")).thenReturn("");
-    when(request.getParameter("term")).thenReturn("select");
-    when(request.getParameter("units")).thenReturn("");
-
-    List<Filter> filters = searchObject.getFilters(request);
+    request = createRequest(request, "", "", "select", "");
 
     List<Filter> expectedFilters = new ArrayList<>();
+
+    List<Filter> filters = searchObject.getFilters(request);
 
     assertEquals(filters, expectedFilters);
   }
 
   @Test
   /* Ensure the correct results are retrieved from Datastore given that all filters are set. */
-  public void getResultsAllFilters() {
+  public void GetResults_AllFiltersSet() {
 
-    DatastoreService db = DatastoreServiceFactory.getDatastoreService();
-    request = Mockito.mock(HttpServletRequest.class);
-
-    when(request.getParameter("courseName")).thenReturn("CS 105");
-    when(request.getParameter("profName")).thenReturn("Smith");
-    when(request.getParameter("term")).thenReturn("Spring 2020");
-    when(request.getParameter("units")).thenReturn("1,2,3");
-
+    request = createRequest(request, "CS 105", "Smith", "Spring 2020", "1,2,3");
     List<Entity> expectedResults = new ArrayList<>();
-    Entity entity1 = new Entity("Course");
-    entity1.setProperty("name", "CS 105");
-    entity1.setProperty("professor", "Smith");
-    entity1.setProperty("term", "Spring 2020");
-    entity1.setProperty("units", 1);
-    db.put(entity1);
-    expectedResults.add(entity1);
 
-    Entity entity2 = new Entity("Course");
-    entity2.setProperty("name", "CS 105");
-    entity2.setProperty("professor", "Smith");
-    entity2.setProperty("term", "Spring 2020");
-    entity2.setProperty("units", 4);
-    db.put(entity2);
+    addCourseEntityWithList("CS 105", "Smith", "Spring 2020", 1, expectedResults, true);
+    addCourseEntityWithList("CS 105", "Smith", "Spring 2020", 4, expectedResults, false);
 
     List<Filter> filters = searchObject.getFilters(request); // Tested in makeFiltersAllParams.
     List<Entity> results = searchObject.getResults(filters);
@@ -139,32 +109,13 @@ public final class SearchServletTest {
 
   @Test
   /* Ensure the correct results are retrieved from Datastore given no search filters. */
-  public void getResultsNoFilters() {
+  public void GetResults_NoFiltersSet() {
 
-    DatastoreService db = DatastoreServiceFactory.getDatastoreService();
-    request = Mockito.mock(HttpServletRequest.class);
-
-    when(request.getParameter("courseName")).thenReturn("");
-    when(request.getParameter("profName")).thenReturn("");
-    when(request.getParameter("term")).thenReturn("select");
-    when(request.getParameter("units")).thenReturn("");
-
+    request = createRequest(request, "", "", "select", "");
     List<Entity> expectedResults = new ArrayList<>();
-    Entity entity1 = new Entity("Course");
-    entity1.setProperty("name", "CS 105");
-    entity1.setProperty("professor", "Smith");
-    entity1.setProperty("term", "Spring 2020");
-    entity1.setProperty("units", 1);
-    db.put(entity1);
-    expectedResults.add(entity1);
 
-    Entity entity2 = new Entity("Course");
-    entity2.setProperty("name", "CS 105");
-    entity2.setProperty("professor", "Smith");
-    entity2.setProperty("term", "Spring 2020");
-    entity2.setProperty("units", 4);
-    db.put(entity2);
-    expectedResults.add(entity2);
+    addCourseEntityWithList("CS 105", "Smith", "Spring 2020", 1, expectedResults, true);
+    addCourseEntityWithList("CS 105", "Smith", "Spring 2020", 4, expectedResults, true);
 
     List<Filter> filters = searchObject.getFilters(request); // Tested in makeFiltersNoParams.
     List<Entity> results = searchObject.getResults(filters);
@@ -174,98 +125,84 @@ public final class SearchServletTest {
 
   @Test
   /* Ensure the whole doGet process works given all search filters are set. */
-  public void getCoursesAllParams() {
+  public void GetCourses_AllParamsSet() {
 
-    DatastoreService db = DatastoreServiceFactory.getDatastoreService();
-    request = Mockito.mock(HttpServletRequest.class);
-
-    when(request.getParameter("courseName")).thenReturn("CS 105");
-    when(request.getParameter("profName")).thenReturn("Smith");
-    when(request.getParameter("term")).thenReturn("Spring 2020");
-    when(request.getParameter("units")).thenReturn("1,2,3");
-
+    request = createRequest(request, "CS 105", "Smith", "Spring 2020", "1,2,3");
     List<Course> expectedCourses = new ArrayList<>();
 
-    Entity entity1 = new Entity("Course");
-    entity1.setProperty("name", "CS 105");
-    entity1.setProperty("professor", "Smith");
-    entity1.setProperty("term", "Spring 2020");
-    entity1.setProperty("units", 1);
-    db.put(entity1);
+    addCourseEntity("CS 105", "Smith", "Spring 2020", 1);
     expectedCourses.add(new Course("CS 105", "Smith", Long.valueOf(1), "Spring 2020"));
 
-    Entity entity2 = new Entity("Course");
-    entity2.setProperty("name", "CS 105");
-    entity2.setProperty("professor", "Smith");
-    entity2.setProperty("term", "Spring 2020");
-    entity2.setProperty("units", 2);
-    db.put(entity2);
+    addCourseEntity("CS 105", "Smith", "Spring 2020", 2);
     expectedCourses.add(new Course("CS 105", "Smith", Long.valueOf(2), "Spring 2020"));
 
-    Entity entity3 = new Entity("Course");
-    entity3.setProperty("name", "CS 106");
-    entity3.setProperty("professor", "Smith");
-    entity3.setProperty("term", "Spring 2020");
-    entity3.setProperty("units", 3);
-    db.put(entity3);
+    addCourseEntity("CS 106", "Smith", "Spring 2020", 3);
 
     List<Filter> filters = searchObject.getFilters(request); // Tested in makeFiltersAllParams.
     List<Entity> results = searchObject.getResults(filters); // Tested in getResultsAllFilters.
     List<Course> courses = searchObject.getCourses(results);
 
-    assertEquals(courses.size(), expectedCourses.size());
-    for (int i = 0; i < courses.size(); i++) {
-      Course actual = courses.get(i);
-      Course expected = expectedCourses.get(i);
-      assertEquals(actual.name, expected.name);
-      assertEquals(actual.professor, expected.professor);
-      assertEquals(actual.units, expected.units);
-      assertEquals(actual.term, expected.term);
-    }
+    assertEqualsCourseArrays(courses, expectedCourses);
   }
 
   @Test
   /* Ensure the whole doGet process works as expected given no search filters. */
-  public void getCoursesNoParams() {
+  public void GetCourses_NoParamsSet() {
 
-    DatastoreService db = DatastoreServiceFactory.getDatastoreService();
-    request = Mockito.mock(HttpServletRequest.class);
-
-    when(request.getParameter("courseName")).thenReturn("");
-    when(request.getParameter("profName")).thenReturn("");
-    when(request.getParameter("term")).thenReturn("select");
-    when(request.getParameter("units")).thenReturn("");
-
+    request = createRequest(request, "", "", "select", "");
     List<Course> expectedCourses = new ArrayList<>();
 
-    Entity entity1 = new Entity("Course");
-    entity1.setProperty("name", "CS 105");
-    entity1.setProperty("professor", "Smith");
-    entity1.setProperty("term", "Spring 2020");
-    entity1.setProperty("units", 1);
-    db.put(entity1);
+    addCourseEntity("CS 105", "Smith", "Spring 2020", 1);
     expectedCourses.add(new Course("CS 105", "Smith", Long.valueOf(1), "Spring 2020"));
 
-    Entity entity2 = new Entity("Course");
-    entity2.setProperty("name", "CS 105");
-    entity2.setProperty("professor", "Smith");
-    entity2.setProperty("term", "Spring 2020");
-    entity2.setProperty("units", 2);
-    db.put(entity2);
+    addCourseEntity("CS 105", "Smith", "Spring 2020", 2);
     expectedCourses.add(new Course("CS 105", "Smith", Long.valueOf(2), "Spring 2020"));
 
-    Entity entity3 = new Entity("Course");
-    entity3.setProperty("name", "CS 106");
-    entity3.setProperty("professor", "Smith");
-    entity3.setProperty("term", "Spring 2020");
-    entity3.setProperty("units", 3);
-    db.put(entity3);
+    addCourseEntity("CS 106", "Smith", "Spring 2020", 3);
     expectedCourses.add(new Course("CS 106", "Smith", Long.valueOf(3), "Spring 2020"));
 
     List<Filter> filters = searchObject.getFilters(request); // Tested in makeFiltersAllParams.
     List<Entity> results = searchObject.getResults(filters); // Tested in getResultsAllFilters.
     List<Course> courses = searchObject.getCourses(results);
 
+    assertEqualsCourseArrays(courses, expectedCourses);
+  }
+
+  private void addCourseEntityWithList(
+      String name, String professor, String term, int units, List<Entity> entities, boolean toAdd) {
+    DatastoreService db = DatastoreServiceFactory.getDatastoreService();
+    Entity entity = new Entity("Course");
+    entity.setProperty("name", name);
+    entity.setProperty("professor", professor);
+    entity.setProperty("units", units);
+    entity.setProperty("term", term);
+    db.put(entity);
+    if (toAdd) {
+      entities.add(entity);
+    }
+  }
+
+  private void addCourseEntity(String name, String professor, String term, int units) {
+    DatastoreService db = DatastoreServiceFactory.getDatastoreService();
+    Entity entity = new Entity("Course");
+    entity.setProperty("name", name);
+    entity.setProperty("professor", professor);
+    entity.setProperty("units", units);
+    entity.setProperty("term", term);
+    db.put(entity);
+  }
+
+  private HttpServletRequest createRequest(
+      HttpServletRequest request, String name, String professor, String term, String units) {
+    request = Mockito.mock(HttpServletRequest.class);
+    when(request.getParameter("courseName")).thenReturn(name);
+    when(request.getParameter("profName")).thenReturn(professor);
+    when(request.getParameter("term")).thenReturn(term);
+    when(request.getParameter("units")).thenReturn(units);
+    return request;
+  }
+
+  private void assertEqualsCourseArrays(List<Course> courses, List<Course> expectedCourses) {
     assertEquals(courses.size(), expectedCourses.size());
     for (int i = 0; i < courses.size(); i++) {
       Course actual = courses.get(i);
