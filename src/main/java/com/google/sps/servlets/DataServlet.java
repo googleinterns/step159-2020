cdadc378e0664366428c1addd66151203cae772c
Dagmawi <dagmawi.s.haile@gmail.com>
2020-08-15 00:05:49 +0000

    removes commented file

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -1,185 +1,181 @@
-// package com.google.sps.servlets;
+package com.google.sps.servlets;
 
-// import com.google.appengine.api.datastore.DatastoreService;
-// import com.google.appengine.api.datastore.DatastoreServiceFactory;
-// import com.google.appengine.api.datastore.Entity;
-// import com.google.appengine.api.datastore.FetchOptions;
-// import com.google.appengine.api.datastore.Key;
-// import com.google.appengine.api.datastore.PreparedQuery;
-// import com.google.appengine.api.datastore.Query;
-// import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
-// import com.google.appengine.api.datastore.Query.Filter;
-// import com.google.appengine.api.datastore.Query.FilterOperator;
-// import com.google.appengine.api.datastore.Query.FilterPredicate;
-// import com.google.appengine.api.datastore.Query.SortDirection;
-// import com.google.cloud.language.v1.Document;
-// import com.google.cloud.language.v1.LanguageServiceClient;
-// import com.google.cloud.language.v1.Sentiment;
-// import com.google.gson.Gson;
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.util.ArrayList;
-// import java.util.List;
-// import javax.servlet.annotation.WebServlet;
-// import javax.servlet.http.HttpServlet;
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
-// import org.json.JSONException;
-// import org.json.JSONObject;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-// /** An item on a todo list. */
-// @WebServlet("/data")
-// public class DataServlet extends HttpServlet {
-//   private final List<Object> commentsList = new ArrayList<>();
-//   private Key currentTermKey;
-//   private LanguageServiceClient languageService;
-//   private final DatastoreService db = DatastoreServiceFactory.getDatastoreService();
+/** An item on a todo list. */
+@WebServlet("/data")
+public class DataServlet extends HttpServlet {
+  private final List<Object> commentsList = new ArrayList<>();
+  private Key currentTermKey;
+  private LanguageServiceClient languageService;
+  private final DatastoreService db = DatastoreServiceFactory.getDatastoreService();
 
-//   // Will re-add constructor later for testing.
+  // Will re-add constructor later for testing.
 
-//   @Override
-//   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
-// {
-//     Query query = new Query("Rating").addSort("score-term", SortDirection.ASCENDING);
-//     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-//     PreparedQuery results = datastore.prepare(query);
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Rating").addSort("score-term", SortDirection.ASCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
 
-//     for (Entity entity : results.asIterable()) {
-//       String comment = (String) entity.getProperty("comments-term");
-//       commentsList.add(comment);
-//     }
+    for (Entity entity : results.asIterable()) {
+      String comment = (String) entity.getProperty("comments-term");
+      commentsList.add(comment);
+    }
 
-//     // Send JSON string.
-//     String jsonVersionCommentsList = new Gson().toJson(commentsList);
-//     commentsList.clear();
-//     response.setContentType("application/json;");
-//     response.getWriter().println(jsonVersionCommentsList);
-//   }
+    // Send JSON string.
+    String jsonVersionCommentsList = new Gson().toJson(commentsList);
+    commentsList.clear();
+    response.setContentType("application/json;");
+    response.getWriter().println(jsonVersionCommentsList);
+  }
 
-//   @Override
-//   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
-// {
-//     // Get written feedback.
-//     addTermRating(request);
-//     response.setContentType("text/html; charset=UTF-8");
-//     response.setCharacterEncoding("UTF-8");
-//   }
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get written feedback.
+    addTermRating(request);
+    response.setContentType("text/html; charset=UTF-8");
+    response.setCharacterEncoding("UTF-8");
+  }
 
-//   public void addTermRating(HttpServletRequest request) throws IOException {
-//     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-//     StringBuilder stringBuilder = new StringBuilder();
-//     String line = null;
-//     try {
-//       BufferedReader reader = request.getReader();
-//       while ((line = reader.readLine()) != null) {
-//         stringBuilder.append(line);
-//       }
-//     } catch (Exception exception) {
-//       /*report an error*/
-//       throw new IOException("Error reading body of request");
-//     }
+  public void addTermRating(HttpServletRequest request) throws IOException {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    StringBuilder stringBuilder = new StringBuilder();
+    String line = null;
+    try {
+      BufferedReader reader = request.getReader();
+      while ((line = reader.readLine()) != null) {
+        stringBuilder.append(line);
+      }
+    } catch (Exception exception) {
+      /*report an error*/
+      throw new IOException("Error reading body of request");
+    }
 
-//     String schoolName = new String();
-//     String courseName = new String();
-//     String profName = new String();
-//     String termName = new String();
-//     Long units = null;
-//     String termFeedback = new String();
-//     String professorFeedback = new String();
-//     Long termRating = null;
-//     Long professorRating = null;
-//     Long workHours = null;
-//     Long difficulty = null;
-//     try {
-//       JSONObject jsonObject = new JSONObject(stringBuilder.toString());
-//       schoolName = jsonObject.getString("schoolName");
-//       courseName = jsonObject.getString("courseName");
-//       profName = jsonObject.getString("profName");
-//       termName = jsonObject.getString("term");
-//       units = (long) jsonObject.getFloat("units");
-//       termFeedback = jsonObject.getString("termInput");
-//       professorFeedback = jsonObject.getString("profInput");
-//       termRating = (long) jsonObject.getFloat("ratingTerm");
-//       professorRating = (long) jsonObject.getFloat("ratingProf");
-//       workHours = (long) jsonObject.getFloat("hours");
-//       difficulty = (long) jsonObject.getFloat("difficulty");
-//     } catch (JSONException exception) {
-//       // If it could not parse string.
-//       throw new IOException("Error parsing JSON request string");
-//     }
+    String schoolName = new String();
+    String courseName = new String();
+    String profName = new String();
+    String termName = new String();
+    Long units = null;
+    String termFeedback = new String();
+    String professorFeedback = new String();
+    Long termRating = null;
+    Long professorRating = null;
+    Long workHours = null;
+    Long difficulty = null;
+    try {
+      JSONObject jsonObject = new JSONObject(stringBuilder.toString());
+      schoolName = jsonObject.getString("schoolName");
+      courseName = jsonObject.getString("courseName");
+      profName = jsonObject.getString("profName");
+      termName = jsonObject.getString("term");
+      units = (long) jsonObject.getFloat("units");
+      termFeedback = jsonObject.getString("termInput");
+      professorFeedback = jsonObject.getString("profInput");
+      termRating = (long) jsonObject.getFloat("ratingTerm");
+      professorRating = (long) jsonObject.getFloat("ratingProf");
+      workHours = (long) jsonObject.getFloat("hours");
+      difficulty = (long) jsonObject.getFloat("difficulty");
+    } catch (JSONException exception) {
+      // If it could not parse string.
+      throw new IOException("Error parsing JSON request string");
+    }
 
-//     float termScore = getSentimentScore(termFeedback);
-//     float professorScore = getSentimentScore(professorFeedback);
+    float termScore = getSentimentScore(termFeedback);
+    float professorScore = getSentimentScore(professorFeedback);
 
-//     // Modifying tests right now to reflect changes.
-//     currentTermKey = findTerm(db, schoolName, courseName, termName, units, profName).getKey();
+    // Modifying tests right now to reflect changes.
+    currentTermKey = findTerm(db, schoolName, courseName, termName, units, profName).getKey();
 
-//     // Check whether user has reviewed that term.
-//     List<Entity> termRatingQueryList = new ArrayList();
+    // Check whether user has reviewed that term.
+    List<Entity> termRatingQueryList = new ArrayList();
 
-//     Entity termRatingEntity =
-//         termRatingQueryList.isEmpty()
-//             ? new Entity("Rating", currentTermKey)
-//             : termRatingQueryList.get(0);
+    Entity termRatingEntity =
+        termRatingQueryList.isEmpty()
+            ? new Entity("Rating", currentTermKey)
+            : termRatingQueryList.get(0);
 
-//     termRatingEntity.setProperty("comments-term", termFeedback);
-//     termRatingEntity.setProperty("score-term", termScore);
-//     termRatingEntity.setProperty("perception-term", termRating);
-//     termRatingEntity.setProperty("hours", workHours);
-//     termRatingEntity.setProperty("difficulty", difficulty);
-//     termRatingEntity.setProperty("comments-professor", professorFeedback);
-//     termRatingEntity.setProperty("score-professor", professorScore);
-//     termRatingEntity.setProperty("perception-professor", professorRating);
-//     datastore.put(termRatingEntity);
-//   }
+    termRatingEntity.setProperty("comments-term", termFeedback);
+    termRatingEntity.setProperty("score-term", termScore);
+    termRatingEntity.setProperty("perception-term", termRating);
+    termRatingEntity.setProperty("hours", workHours);
+    termRatingEntity.setProperty("difficulty", difficulty);
+    termRatingEntity.setProperty("comments-professor", professorFeedback);
+    termRatingEntity.setProperty("score-professor", professorScore);
+    termRatingEntity.setProperty("perception-professor", professorRating);
+    datastore.put(termRatingEntity);
+  }
 
-//   private float getSentimentScore(String feedback) throws IOException {
-//     LanguageServiceClient languageService = LanguageServiceClient.create();
-//     Document feedbackDoc =
-//         Document.newBuilder().setContent(feedback).setType(Document.Type.PLAIN_TEXT).build();
-//     Sentiment sentiment = languageService.analyzeSentiment(feedbackDoc).getDocumentSentiment();
-//     float score = sentiment.getScore();
-//     languageService.close();
-//     return score;
-//   }
+  private float getSentimentScore(String feedback) throws IOException {
+    LanguageServiceClient languageService = LanguageServiceClient.create();
+    Document feedbackDoc =
+        Document.newBuilder().setContent(feedback).setType(Document.Type.PLAIN_TEXT).build();
+    Sentiment sentiment = languageService.analyzeSentiment(feedbackDoc).getDocumentSentiment();
+    float score = sentiment.getScore();
+    languageService.close();
+    return score;
+  }
 
-//   private Entity findTerm(
-//       DatastoreService db,
-//       String schoolName,
-//       String courseName,
-//       String termName,
-//       Long units,
-//       String profName)
-//       throws IOException {
-//     Key schoolKey = queryEntities("School", "school-name", schoolName).get(0).getKey();
+  private Entity findTerm(
+      DatastoreService db,
+      String schoolName,
+      String courseName,
+      String termName,
+      Long units,
+      String profName)
+      throws IOException {
+    Key schoolKey = queryEntities("School", "school-name", schoolName).get(0).getKey();
 
-//     List<Filter> filters = new ArrayList();
-//     Filter courseFilter = new FilterPredicate("course-name", FilterOperator.EQUAL, courseName);
-//     Filter unitFilter = new FilterPredicate("units", FilterOperator.EQUAL, units);
-//     filters.add(courseFilter);
-//     filters.add(unitFilter);
+    List<Filter> filters = new ArrayList();
+    Filter courseFilter = new FilterPredicate("course-name", FilterOperator.EQUAL, courseName);
+    Filter unitFilter = new FilterPredicate("units", FilterOperator.EQUAL, units);
+    filters.add(courseFilter);
+    filters.add(unitFilter);
 
-//     Query courseQuery =
-//         new
-// Query("Course").setAncestor(schoolKey).setFilter(CompositeFilterOperator.and(filters));
-//     Key courseKey =
-//         db.prepare(courseQuery).asList(FetchOptions.Builder.withDefaults()).get(0).getKey();
+    Query courseQuery =
+        new Query("Course").setAncestor(schoolKey).setFilter(CompositeFilterOperator.and(filters));
+    Key courseKey =
+        db.prepare(courseQuery).asList(FetchOptions.Builder.withDefaults()).get(0).getKey();
 
-//     Filter termFilter = new FilterPredicate("term", FilterOperator.EQUAL, termName);
-//     Query termQuery = new Query("Term").setAncestor(courseKey).setFilter(termFilter);
-//     Entity foundTerm = db.prepare(termQuery).asList(FetchOptions.Builder.withDefaults()).get(0);
+    Filter termFilter = new FilterPredicate("term", FilterOperator.EQUAL, termName);
+    Query termQuery = new Query("Term").setAncestor(courseKey).setFilter(termFilter);
+    Entity foundTerm = db.prepare(termQuery).asList(FetchOptions.Builder.withDefaults()).get(0);
 
-//     return foundTerm;
-//   }
+    return foundTerm;
+  }
 
-//   public List<Entity> queryEntities(String entityName, String propertyName, String propertyValue)
-//       throws IOException {
-//     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-//     Filter filter = new FilterPredicate(propertyName, FilterOperator.EQUAL, propertyValue);
-//     Query query = new Query(entityName).setFilter(filter);
-//     // This is initialized when authentication happens, so should not be empty.
-//     List<Entity> queryList =
-// datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
-//     return queryList;
-//   }
-// }
+  public List<Entity> queryEntities(String entityName, String propertyName, String propertyValue)
+      throws IOException {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Filter filter = new FilterPredicate(propertyName, FilterOperator.EQUAL, propertyValue);
+    Query query = new Query(entityName).setFilter(filter);
+    // This is initialized when authentication happens, so should not be empty.
+    List<Entity> queryList = datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
+    return queryList;
+  }
+}
