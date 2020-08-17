6737ceb10c31cd84621308139dca6a5214c3d53d
Dagmawi <dagmawi.s.haile@gmail.com>
2020-08-17 23:04:43 +0000

    refactors livecoursedata to work with new params of termKey and coursekey

--- src/main/java/com/google/sps/servlets/LiveCourseData.java
+++ src/main/java/com/google/sps/servlets/LiveCourseData.java
@@ -7,10 +7,6 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.sps.data.TermDataHolder;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -35,7 +31,8 @@ public class LiveCourseData extends HttpServlet {
 
   public TermDataHolder getAllDataFromTerm(DatastoreService db, HttpServletRequest request) {
     TermDataHolder termDataHolder = new TermDataHolder();
-    Entity foundTerm = getTerm(db, request);
+    Key termKey = KeyFactory.stringToKey(request.getProperty("term-key"));
+    Key courseKey = KeyFactory.stringToKey(request.getProperty("course-key"));
 
     termDataHolder.setHoursList(getDataFromTermRating(db, foundTerm, "hours"));
     termDataHolder.setDifficultyList(getDataFromTermRating(db, foundTerm, "difficulty"));
@@ -50,60 +47,14 @@ public class LiveCourseData extends HttpServlet {
     return termDataHolder;
   }
 
-  public Entity getTerm(DatastoreService db, HttpServletRequest request) {
-    String schoolName = request.getParameter("school-name");
-    String courseName = request.getParameter("course-name");
-    String termName = request.getParameter("term");
-    String profName = request.getParameter("prof-name");
-    Long units = Long.parseLong(request.getParameter("num-units"));
-
-    Entity foundTerm = findTerm(db, schoolName, courseName, termName, units, profName);
-    return foundTerm;
-  }
-
-  private List<Object> getDataFromTermRating(DatastoreService db, Entity term, String property) {
+  private List<Object> getDataFromTermRating(DatastoreService db, Key termKey, String property) {
     List<Object> dataList = new ArrayList();
-    List<Entity> termRatings = findChildren(db, "Rating", term.getKey());
+    List<Entity> termRatings = findChildren(db, "Rating", termKey);
 
     for (Entity rating : termRatings) {
       dataList.add(Arrays.asList(rating.getProperty(property)));
     }
     return dataList;
-  }
-
-  private Entity findTerm(
-      DatastoreService db,
-      String schoolName,
-      String courseName,
-      String termName,
-      Long units,
-      String profName) {
-    Key schoolKey = findQueryMatch(db, "School", "school-name", schoolName).get(0).getKey();
-
-    List<Filter> filters = new ArrayList();
-    Filter courseFilter = new FilterPredicate("course-name", FilterOperator.EQUAL, courseName);
-    Filter unitFilter = new FilterPredicate("units", FilterOperator.EQUAL, units);
-    filters.add(courseFilter);
-    filters.add(unitFilter);
-
-    Query courseQuery =
-        new Query("Course").setAncestor(schoolKey).setFilter(CompositeFilterOperator.and(filters));
-    Key courseKey =
-        db.prepare(courseQuery).asList(FetchOptions.Builder.withDefaults()).get(0).getKey();
-
-    Filter termFilter = new FilterPredicate("term", FilterOperator.EQUAL, termName);
-    Query termQuery = new Query("Term").setAncestor(courseKey).setFilter(termFilter);
-    Entity foundTerm = db.prepare(termQuery).asList(FetchOptions.Builder.withDefaults()).get(0);
-
-    return foundTerm;
-  }
-
-  private List<Entity> findQueryMatch(
-      DatastoreService db, String entityType, String entityProperty, String propertyValue) {
-    Filter filter = new FilterPredicate(entityProperty, FilterOperator.EQUAL, propertyValue);
-    Query q = new Query(entityType).setFilter(filter);
-    List<Entity> result = db.prepare(q).asList(FetchOptions.Builder.withDefaults());
-    return result;
   }
 
   private List<Entity> findChildren(DatastoreService db, String type, Key parent) {
