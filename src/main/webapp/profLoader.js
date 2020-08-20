eb59e6316a695897b48010cf719d3da2d9ebc516
Dagmawi <dagmawi.s.haile@gmail.com>
2020-08-20 23:35:56 +0000

    save

--- src/main/webapp/profLoader.js
+++ src/main/webapp/profLoader.js
@@ -8,16 +8,71 @@
   const profKey = urlParams.get("prof-key");
   const profName = urlParams.get("prof-name");
 
-  function fillTitles() {
+  function populateData() {
     fetch(`/prof-data?prof-key=${profKey}`)
       .then((response) => response.json())
       .then((profInfo) => {
         document.getElementById("prof-name").innerHTML = profName;
-        console.log(profInfo);
+        makeDiffComparisonChart(profInfo);
       });
   }
 
   document.addEventListener("DOMContentLoaded", function () {
-    fillTitles();
+    populateData();
   });
+
+  async function makeDiffComparisonChart(profData) {
+    const average = (list) =>
+      list.reduce((prev, curr) => prev + curr) / list.length;
+
+    const courseList = [];
+    const diffAvgList = [];
+
+    for (let termHolder of profData) {
+      courseList.push(termHolder.properties.term);
+      diffAvgList.push(average(termHolder.properties.difficultyList));
+    }
+
+    console.log(courseList);
+    console.log(diffAvgList);
+
+    // const currentPerceptionRatingAvg = average(
+    //   /* adds dummy data */ [21, 11, 9].concat(
+    //     termDataObject.termPerceptionList
+    //   )
+    // );
+
+    // const prevTermData = [];
+    // const avgData = [];
+    // const prevTermNameList = await getPrevTermName(2);
+
+    // for (let term of prevTermNameList) {
+    //   prevTermData.push(await getPreviousTermData(term));
+    // }
+
+    // for (let termData of prevTermData) {
+    //   avgData.push(average(termData.termPerceptionList.concat([17, 8, 5])));
+    // }
+
+    // const perceptionData = google.visualization.arrayToDataTable([
+    //   [" ", document.getElementById("term-name").innerHTML].concat(
+    //     prevTermNameList
+    //   ),
+    //   [" ", currentPerceptionRatingAvg].concat(avgData),
+    // ]);
+
+    // const options = {
+    //   colors: ["#81b8ec", "#f1a79d", "#f1d19d"],
+    //   title: "Average Term Perception Comparison",
+    //   height: 450,
+    //   bars: "horizontal",
+    //   bar: { groupWidth: "30%" },
+    //   hAxis: { title: "Avg Term Perception" },
+    // };
+
+    // const chart = new google.charts.Bar(
+    //   document.getElementById("term-perception-comp")
+    // );
+    // chart.draw(perceptionData, google.charts.Bar.convertOptions(options));
+  }
 })();
