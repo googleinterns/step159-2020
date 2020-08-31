46cb9dbfc6fd3d9ae022621f9394b41ca11e6888
Laura <lqueipom@gmail.com>
2020-08-31 14:55:07 +0000

    adding more tests

--- src/main/webapp/ratingScript.js
+++ src/main/webapp/ratingScript.js
@@ -59,7 +59,7 @@ function addingAttributesToURL(ratingProperties) {
 }
 
 async function passRatingProperties() {
-  document.getElementById("message-rating").innerHTML = "";
+  document.getElementById("retrieve-last-rating-message").innerHTML = "";
   const urlAndData = await getRatingPropertiesToStore();
   postRatingProperties(urlAndData[0], urlAndData[1]);
 }
@@ -69,11 +69,11 @@ $(function () {
 });
 
 async function getLatestRating() {
-  let messageRetrievalElement = document.getElementById("retrieve-last-rating-message")
-  messageRetrievalElement
-    .classList.remove("alert-light");
-  messageRetrievalElement
-    .classList.add("alert-secondary");
+  let messageRetrievalElement = document.getElementById(
+    "retrieve-last-rating-message"
+  );
+  messageRetrievalElement.classList.remove("alert-light");
+  messageRetrievalElement.classList.add("alert-secondary");
   messageRetrievalElement.innerHTML = "Fetching Rating...";
   const userId = await verify();
   const queryString = window.location.search;
@@ -87,10 +87,8 @@ async function getLatestRating() {
   const formInfo = await response.json();
 
   if (Object.keys(formInfo).length == 0) {
-    messageRetrievalElement
-      .classList.remove("alert-secondary");
-    messageRetrievalElement
-      .classList.add("alert-danger");
+    messageRetrievalElement.classList.remove("alert-secondary");
+    messageRetrievalElement.classList.add("alert-danger");
     messageRetrievalElement.innerHTML =
       "You have not submitted a rating for this term";
   } else {
@@ -102,12 +100,9 @@ async function getLatestRating() {
     document.getElementById("difficulty").value = formInfo["difficulty"];
     document.getElementById("grade").value = formInfo["grade"];
     document.getElementById("translate").value = formInfo["translation"];
-    
-    messageRatingElement
-      .classList.remove("alert-secondary");
-    messageRatingElement
-      .classList.add("alert-success");
-    messageRetrievalElement.innerHTML =
-      "Your form has been populated!";
+
+    messageRatingElement.classList.remove("alert-secondary");
+    messageRatingElement.classList.add("alert-success");
+    messageRetrievalElement.innerHTML = "Your form has been populated!";
   }
 }
