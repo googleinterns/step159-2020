ee91428394c48a077c1652b4720da181d46af2c3
Dagmawi <dagmawi.s.haile@gmail.com>
2020-09-01 21:12:22 +0000

    data save

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -42,7 +42,6 @@ public class DataServlet extends HttpServlet {
   private final List<Object> commentsList = new ArrayList<>();
   private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
   private LanguageServiceClient languageService;
-  private TranslationServiceClient translateService;
 
   public DataServlet() throws IOException {
     this.languageService = LanguageServiceClient.create();
@@ -259,17 +258,22 @@ public class DataServlet extends HttpServlet {
 
   private String translateTextToEnglish(String text) throws IOException {
     String projectId = "nina-laura-dagm-step-2020";
-    LocationName parent = LocationName.of(projectId, "global");
+    try (TranslationServiceClient client = TranslationServiceClient.create()) {
+      LocationName parent = LocationName.of(projectId, "global");
 
-    TranslateTextRequest request =
-        TranslateTextRequest.newBuilder()
-            .setParent(parent.toString())
-            .setMimeType("text/plain")
-            .setTargetLanguageCode("en")
-            .addContents(text)
-            .build();
+      TranslateTextRequest request =
+          TranslateTextRequest.newBuilder()
+              .setParent(parent.toString())
+              .setMimeType("text/plain")
+              .setTargetLanguageCode("en")
+              .addContents(text)
+              .build();
 
-    TranslateTextResponse response = this.translateService.translateText(request);
-    return response.getTranslationsList().get(0).getTranslatedText();
+      TranslateTextResponse response = client.translateText(request);
+      return response.getTranslationsList().get(0).getTranslatedText();
+
+    } catch (IOException exception) {
+      throw new IOException("Could not translate comments");
+    }
   }
 }
